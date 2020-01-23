c43e1f8f1eef8739dc3a0e47991062d368cb4f13
Alisson <aafonsoamaral@gmail.com>
2020-01-22 22:29:40 -0300

    Criado a função cumprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,7 +1,16 @@
 class Pessoa:
+
+    def __init__(self, *filhos, nome = None):
+        self.nome = nome
+        self.filhos = list(filhos)
+
     def cumprimentar(self):
         return 'Olá mundo'
 
 if __name__ == '__main__':
-    p = Pessoa()
-    print(p.cumprimentar())
+    afonso = Pessoa(nome='Afonso')
+    alisson = Pessoa(afonso, nome='Alisson')
+
+    print(alisson.filhos)
+    for filho in afonso.filhos:
+        print(filho.nome)
